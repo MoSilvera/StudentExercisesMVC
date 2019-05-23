ae05654d680cfbd4deca047bd71ec99839423bcf
Mo Silvera <mo.leslie.silvera@gmail.com>
2019-05-23 12:33:27 -0700

    working details view cohorts

---
+++ StudentExerciseMVC3/Controllers/CohortsController.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using StudentExerciseMVC3.Models.ViewModels;
+using StudentExercisesAPI.Models;
+
+namespace StudentExerciseMVC3.Controllers
+{
+    public class CohortsController : Controller
+    {
+        private readonly IConfiguration _config;
+
+        public CohortsController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        // GET: Students
+        public ActionResult Index()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT c.Id,
+                        c.Designation
+                        FROM Cohort c;
+                        ";
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    List<Cohort> cohorts = new List<Cohort>();
+                    while (reader.Read())
+                    {
+                        Cohort cohort = new Cohort
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Designation = reader.GetString(reader.GetOrdinal("Designation"))
+                            
+                            
+                        };
+
+                        cohorts.Add(cohort);
+                    }
+
+                    reader.Close();
+
+                    return View(cohorts);
+                }
+            }
+        }
+
+        // GET: Students/Details/5
+        public ActionResult Details(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText =
+                        $@"
+                        SELECT
+                        c.Id,
+                        c.Designation
+                        FROM Cohort c
+                        Where c.id = @id;";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    Cohort cohort = null;
+
+                    if (reader.Read())
+                    {
+                        cohort = new Cohort
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Designation = reader.GetString(reader.GetOrdinal("Designation"))
+
+                        };
+
+                    }
+                    reader.Close();
+                    return View(cohort);
+
+                }
+            }
+            
+        }
+
+        // GET: Students/Create
+        public ActionResult Create()
+        {
+           
+            return View();
+        }
+
+        // POST: Students/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([FromForm] Exercise exercise)
+        {
+            try
+            {
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"INSERT INTO Exercise (Title, Lang)
+                                            OUTPUT INSERTED.Id
+                                            VALUES (@Title, @Lang)";
+                        cmd.Parameters.Add(new SqlParameter("@Title", exercise.Title));
+                        cmd.Parameters.Add(new SqlParameter("@Lang", exercise.Lang));
+                        
+
+                        int newId = (int)cmd.ExecuteScalar();
+                        exercise.Id = newId;
+                        return RedirectToAction(nameof(Index));
+                    }
+                }
+
+                
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Students/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Students/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"UPDATE Exercise
+                                                SET Title = @title,
+                                                    Lang = @lang
+                                                WHERE Id = @id";
+                        cmd.Parameters.Add(new SqlParameter("@title", Convert.ToString(collection["Title"])));
+                        cmd.Parameters.Add(new SqlParameter("@lang", Convert.ToString(collection["Lang"])));
+                        cmd.Parameters.Add(new SqlParameter("@id", id));
+                        cmd.ExecuteNonQuery();
+                        return RedirectToAction(nameof(Index));
+
+                    }
+                }
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+                return View();
+            }
+        }
+
+        // GET: Students/Delete/5
+        public ActionResult Delete(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText =
+                        $@"
+                        SELECT
+                        e.Id,
+                        e.Title,
+                        e.Lang
+                        FROM Exercise e
+                        Where e.id = @id;";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    Exercise exercise = null;
+
+                    if (reader.Read())
+                    {
+                        exercise = new Exercise
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Title = reader.GetString(reader.GetOrdinal("Title")),
+                            Lang = reader.GetString(reader.GetOrdinal("Lang"))
+
+                        };
+
+                    }
+                    reader.Close();
+                    return View(exercise);
+
+                }
+            }
+        }
+
+        // POST: Students/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                using (SqlConnection conn = Connection)
+                {
+
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"DELETE FROM StudentExercises
+                                            Where ExerciseId = @id";
+                        cmd.Parameters.Add(new SqlParameter("@id", id));
+                        cmd.ExecuteNonQuery();
+                    }
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"DELETE FROM Exercise WHERE Id = @id";
+                        cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                        cmd.ExecuteNonQuery();
+                        return RedirectToAction(nameof(Index));
+
+                    }
+                }
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        private bool StudentExists(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                            SELECT Id, FirstName, LastName, SlackHandle, CohortId
+                            FROM Student
+                            WHERE Id = @id";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    return reader.Read();
+                }
+            }
+        }
+    }
+}
